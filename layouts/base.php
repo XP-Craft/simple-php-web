1943bdb3d1c6375e3e43031845db3aefc2f68aba
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-07 12:26:20 +0200

    Create base layouts

---
+++ layouts/base.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><? $config['site_name'] ?></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php include __DIR__ . '/components/navbar.php'; ?>
+
+<?php include __DIR__ . '/components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+</body>
+</html>
