1250ab3c0c70393cfa7e6fc5e29f8dcb59e9f17c
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-07 13:55:05 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,22 @@
 <?php
-// config.php
 
 return [
     'site_name' => 'Simple PHP Web',
-    'pricings' => [],
+    'pricings' => [
+        [
+            'title' => 'Basic',
+            'price' => '$10',
+            'features' => ['Feature A', 'Feature B'],
+        ],
+        [
+            'title' => 'Pro',
+            'price' => '$25',
+            'features' => ['Feature A', 'Feature B', 'Feature C'],
+        ],
+        [
+            'title' => 'Enterprise',
+            'price' => '$50',
+            'features' => ['All Features'],
+        ]
+    ],
 ];
