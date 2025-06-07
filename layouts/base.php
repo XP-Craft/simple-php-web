4da45fb5e7cf19cf5ed49cc4cd5ab13574fcb5a0
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-07 12:33:08 +0200

    Update base.php

--- layouts/base.php
+++ layouts/base.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?php echo($config['site_name']); ?></title>
+    <title><?= $config['site_name'] ?></title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
 <?php include __DIR__ . '/components/navbar.php'; ?>
+
+<main>
+    <?php include $view; ?>
+</main>
 
 <?php include __DIR__ . '/components/footer.php'; ?>
 
