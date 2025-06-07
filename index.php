f276b803e908404065ab2b0b40e3e6bca111d7de
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-07 12:32:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 
 $config = include 'config.php';
 
+$page = $_GET['page'] ?? 'home';
+
+switch ($page) {
+    case 'pricing':
+        $view = 'views/pricing.php';
+        break;
+    case 'home':
+    default:
+        $view = 'views/home.php';
+        break;
+}
+
 include 'layouts/base.php';
 
 ?>
