158eac146a7a7c58a293ca159f805c23bacc7ef0
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 11:33:03 +0200

    Create about.php

---
+++ views/about.php
@@ -0,0 +1,3 @@
+<h1>About Us</h1>
+<p>This is a simple website built with plain PHP to show how powerful and clean PHP can be without using a framework.</p>
+<p>Perfect for beginners and those who want full control over their site.</p>
