3cf4df0eee92c77d85a30d721bea37c9e00240f2
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 11:54:29 +0200

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'home' => [
+        'view' => 'views/home.php',
+        'title' => 'Home'
+    ],
+    'pricing' => [
+        'view' => 'views/pricing.php',
+        'title' => 'Pricing'
+    ],
+    'about' => [
+        'view' => 'views/about.php',
+        'title' => 'About'
+    ],
+    'contact' => [
+        'view' => 'views/contact.php',
+        'title' => 'Contact'
+    ],
+    'faq' => [
+        'view' => 'views/faq.php',
+        'title' => 'FAQ'
+    ],
+];
