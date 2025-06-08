aa3ce20ad48a80e3fb2d70a42dc9fabe154f5a66
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 11:45:48 +0200

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -1,7 +1,7 @@
 <nav>
-    <a href="?page=home">Home</a>
-    <a href="?page=pricing">Pricing</a>
-    <a href="?page=about">About</a>
-    <a href="?page=contact">Contact</a>
-    <a href="?page=faq">FAQ</a>
+    <a href="?page=home" <?php if ($current_page === 'home') echo 'class="active"'; ?>>Home</a>
+    <a href="?page=pricing" <?php if ($current_page === 'pricing') echo 'class="active"'; ?>>Pricing</a>
+    <a href="?page=about" <?php if ($current_page === 'about') echo 'class="active"'; ?>>About</a>
+    <a href="?page=contact" <?php if ($current_page === 'contact') echo 'class="active"'; ?>>Contact</a>
+    <a href="?page=faq" <?php if ($current_page === 'faq') echo 'class="active"'; ?>>FAQ</a>
 </nav>
