cdec6764abb7717a528f19000213b199c870078d
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 11:53:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <?php
 $config = include 'config.php';
-
-// Define available routes with metadata
-$routes = [
-    'home' => [
-        'view' => 'views/home.php',
-        'title' => 'Home'
-    ],
-    'pricing' => [
-        'view' => 'views/pricing.php',
-        'title' => 'Pricing'
-    ],
-    'about' => [
-        'view' => 'views/about.php',
-        'title' => 'About Us'
-    ],
-    'contact' => [
-        'view' => 'views/contact.php',
-        'title' => 'Contact'
-    ],
-    'faq' => [
-        'view' => 'views/faq.php',
-        'title' => 'FAQ'
-    ],
-];
+$routes = include 'routes.php';
 
 $current_page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
@@ -31,10 +8,9 @@ if (array_key_exists($current_page, $routes)) {
     $view = $routes[$current_page]['view'];
     $page_title = $routes[$current_page]['title'];
 } else {
-    // fallback to 404 or home
-    $view = 'views/home.php';
-    $page_title = 'Home';
-    $current_page = 'home';
+    // Fallback to 404 Error view/page
+    $view = 'views/404.php';
+    $page_title = 'Page Not Found';
 }
 
 include 'layouts/base.php';
