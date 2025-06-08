e501f37c5ea7975e48eefaff5a12db29018ea31d
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 11:36:14 +0200

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,7 @@
 <nav>
     <a href="?page=home">Home</a>
     <a href="?page=pricing">Pricing</a>
+    <a href="?page=about">About</a>
+    <a href="?page=contact">Contact</a>
+    <a href="?page=faq">FAQ</a>
 </nav>
