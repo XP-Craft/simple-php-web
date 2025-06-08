6921a5ca55cb08e0a53c6b079efc2953b4d718e7
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 12:17:36 +0200

    Update 404.php

--- views/404.php
+++ views/404.php
@@ -1,3 +1,5 @@
-<h1>404 - Page Not Found</h1>
-<p>Sorry, the page you are looking for does not exist.</p>
-<p><a href="?page=home">Return to Home</a></p>
+<div class="error">
+    <h1>404</h1>
+    <p>Sorry, the page you're looking for does not exist.</p>
+    <a href="?page=home">Return to Home</a>
+</div>
