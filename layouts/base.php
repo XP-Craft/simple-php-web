d0ba31f24233c1330c72c3f14afaeb755971750d
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 11:49:00 +0200

    Update base.php

--- layouts/base.php
+++ layouts/base.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?php echo $config['site_name']; ?></title>
+    <title><?php echo $page_title . ' x ' . $config['site_name']; ?></title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
