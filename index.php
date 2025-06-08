751c919f54f8951ee3dfefb51faac546071d190b
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-08 11:47:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,40 @@
 <?php
-
 $config = include 'config.php';
 
-$page = $_GET['page'] ?? 'home';
+// Define available routes with metadata
+$routes = [
+    'home' => [
+        'view' => 'views/home.php',
+        'title' => 'Home'
+    ],
+    'pricing' => [
+        'view' => 'views/pricing.php',
+        'title' => 'Pricing'
+    ],
+    'about' => [
+        'view' => 'views/about.php',
+        'title' => 'About Us'
+    ],
+    'contact' => [
+        'view' => 'views/contact.php',
+        'title' => 'Contact'
+    ],
+    'faq' => [
+        'view' => 'views/faq.php',
+        'title' => 'FAQ'
+    ],
+];
 
 $current_page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
-switch ($page) {
-    case 'pricing':
-        $view = 'views/pricing.php';
-        break;
-    case 'about':
-        $view = 'views/about.php';
-        break;
-    case 'contact':
-        $view = 'views/contact.php';
-        break;
-    case 'faq':
-        $view = 'views/faq.php';
-        break;
-    case 'home':
-    default:
-        $view = 'views/home.php';
-        break;
+if (array_key_exists($current_page, $routes)) {
+    $view = $routes[$current_page]['view'];
+    $page_title = $routes[$current_page]['title'];
+} else {
+    // fallback to 404 or home
+    $view = 'views/home.php';
+    $page_title = 'Home';
+    $current_page = 'home';
 }
 
 include 'layouts/base.php';
