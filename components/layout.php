eff45e7582df048aba8448fb0da332a52b0d8248
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-11 21:57:17 +0200

    Update layout.php

--- components/layout.php
+++ components/layout.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
     <title><?php echo $page_title . ' x ' . $config['site_name']; ?></title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-<?php include __DIR__ . '/../components/alert.php'; ?>
-
-<?php include __DIR__ . '/../components/navbar.php'; ?>
+<?php include __DIR__ . '/navbar.php'; ?>
 
 <main>
     <?php include __DIR__ . '/../' . $view; ?>
 </main>
 
-<?php include __DIR__ . '/../components/footer.php'; ?>
+<?php include __DIR__ . '/footer.php'; ?>
 
 <script src="js/script.js"></script>
 </body>
