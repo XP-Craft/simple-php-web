35365d47a2f3e1383eea8f26d04c3d2b64d8de8b
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-12 07:37:21 +0200

    Update alert component | added alert url support

--- components/alert.php
+++ components/alert.php
@@ -1,4 +1,13 @@
 <?php
+// dynamic alerts via URL
+if (isset($_GET['alert_type'], $_GET['alert_message'])) {
+    $config['alerts'][] = [
+        'type' => $_GET['alert_type'],
+        'message' => $_GET['alert_message'],
+    ];
+}
+
+// display alerts
 if (!empty($config['alerts'])):
     foreach ($config['alerts'] as $alert):
         $type = $alert['type'];
